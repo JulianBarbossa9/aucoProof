5e432980faf8966a05237f0e0b676d4074720b6a
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-10 18:12:30 -0500

    add component postDetail

---
+++ app/[post]/page.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import React from 'react'
+
+const PostDetail = async ({ params } : any) => {
+
+  const { post } = params
+
+  console.log({post})
+  // console.log({params})
+
+  const url:string = `https://jsonplaceholder.typicode.com/posts/${post}`
+
+  try {
+    const data = await fetch(url)
+    const response = await data.json()
+
+    return (
+      <>
+      <div className="m-32 ">
+        {/* <button className='bg-sky-500 px-4 py-3 rounded'> */}
+          <Link href={'/'} className='bg-indigo-500  px-4 py-3 rounded '>
+            Home
+          </Link>
+        {/* </button> */}
+          <div className=' w-50  py-4 px-8 bg-white shadow-lg rounded-lg my-2 mt-10'>
+            <div className='p-10'>
+              <h2 className='text-gray-800 text-3xl font-semibold uppercase mb-5'>{ response.title}</h2>
+              <hr />
+              <p className='text-black mt-5 normal-case'>{response.body}</p>
+              
+            </div>
+          </div>
+      </div>
+      
+      </>
+    )
+  } catch (error) {
+    
+  }
+  return (
+    <div>PostDetail</div>
+  )
+}
+
+export default PostDetail
