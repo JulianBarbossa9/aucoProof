b322ff75b7da7d3d00e443f961a45f474e7a38a2
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-10 22:49:25 -0500

    add dynamic route for the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,39 @@
-import Image from 'next/image'
+"use client"
+
 import Post from './components/Post'
 import { PostI } from './interface/postInterface'
 import Link from 'next/link'
+import { PostContext } from '@/context/PostProvider'
+import usePost from '@/hooks/usePost'
+import PaginationControls from './components/PaginationControls'
 
 
 
-export default async function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  const { listPost } = usePost()
 
-  const url:string = 'https://jsonplaceholder.typicode.com/posts'
-  const data = await fetch(url)
-  const res = await data.json()
-  // console.log(res)
+  const page = searchParams['page'] ?? '1'
+  const per_page = searchParams['per_page'] ?? '5'
+
+  const start = (Number(page) - 1) * Number(per_page) // 0, 5, 10 ...
+  const end = start + Number(per_page) // 5, 10, 15 ...
+
+  const entries = listPost.slice(start, end)
+
+
+
+console.log({listPost})
 
   return (
     <main className='flex flex-col justify-center align-middle'>
       <div className='mt-10'>
         <h1 className=' text-center text-7xl capitalize  font-extrabold mb-5'>List of Post</h1>
         <hr />
-        <div className='mt-5'>
+        <div className='mt-5 flex justify-between'>
           <Link href={'/'} className='text-4xl bold hover:underline  
               hover:extrabold
               inline-block 
@@ -25,16 +41,21 @@ export default async function Home() {
               cursor-pointer'>
             Home
           </Link>
+          <PaginationControls 
+            hasNextPage={end < listPost.length}
+            hasPrevPage={start > 0}
+          />
         </div>
       </div>
         <div className='grid gap-16 grid-cols-fluid mt-20'>
         {
-          res.map((data: PostI) => (
+          entries.map((data: PostI) => (
             <Post 
               userId= {data.userId}
               id= {data.id}
               title= {data.title}
               body= {data.body}
+              key={data.id}
             />
           ))
         }
